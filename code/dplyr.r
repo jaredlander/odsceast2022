f7ba539bba1d518fac0a98163b2f4ecff10d5a4c
Jared P. Lander <jared@landeranalytics.com>
2022-04-18 12:35:41 -0400

    Showed filter

--- code/dplyr.r
+++ code/dplyr.r
@@ -3,8 +3,12 @@ library(readr)
 
 # sum(x)
 
+# Reading data ####
+
 house <- read_csv('https://jaredlander.com/data/housing.csv')
 house
+
+# Selecting Columns ####
 
 house <- house |>
     select(
@@ -24,3 +28,41 @@ house |> select(-Class)
 house |>
     select(where(is.character))
 house
+
+# Slicing Rows ####
+
+house |> slice(5, 10, 12)
+house |> slice(-1)
+
+# Filtering Rows ####
+
+house |>
+    filter(Units <= 100)
+
+house
+
+house |>
+    filter(Units <= 100 & SqFt <= 50000)
+
+house |>
+    filter(Units > 100 | SqFt > 100000)
+
+house |>
+    filter(Boro == 'Brooklyn')
+
+house |>
+    filter(Boro == 'Brooklyn' & Units >= 100)
+
+house |>
+    filter(Boro == 'Brooklyn' | Boro == 'Queens')
+
+house |>
+    filter(Boro == 'Brooklyn' | Boro == 'Queens' | Boro == 'Manhattan')
+
+house |>
+    filter(Boro %in% c('Brooklyn', 'Queens', "Manhattan"))
+
+house |>
+    filter(Boro %in% c('Brooklyn', 'Queens') & Units <= 100)
+
+
